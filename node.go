467e07dc732a279ab1a2628d63318a33b3ca3ea6
marsevilspirit <marsevilspirit@gmail.com>
2025-07-18 14:40:20 +0800

    feat(node): add more field

--- node.go
+++ node.go
@@ -1,9 +1,10 @@
 package deimosclient
 
 type Node struct {
-	Key           string `json:"key"`
-	Value         string `json:"value,omitempty"`
-	Dir           bool   `json:"dir,omitempty"`
-	ModifiedIndex uint64 `json:"modifiedIndex"`
-	CreatedIndex  uint64 `json:"createdIndex"`
+	Key           string  `json:"key"`
+	Value         string  `json:"value,omitempty"`
+	Dir           bool    `json:"dir,omitempty"`
+	Nodes         []*Node `json:"nodes,omitempty"`
+	ModifiedIndex uint64  `json:"modifiedIndex"`
+	CreatedIndex  uint64  `json:"createdIndex"`
 }
